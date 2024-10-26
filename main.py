8c51ffda31759ff60d2b2fe25076c0ba7cc71586
root <root@DESKTOP-GLHS38H>
2024-10-26 14:20:09 -0300

    adding options to choose LLM and embedder model;

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import os
 import argparse
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_community.llms import OpenAI
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -12,28 +11,40 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter #split text 
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", required=True, choices=["GPT", "Gemini"],
-                    help="Choose between OpenAI and Google")
+parser.add_argument("--modelFamily", required=True, choices=["GPT", "Gemini"],
+                    help="Choose between GPT and Gemini")
+parser.add_argument("--LLMModel", required=False,
+                    help="LLM model from chosen family (more information in README)")
+parser.add_argument("--EmbeddingModel", required=False,
+                    help="Embedding model from chosen family (more information in README)")
 parser.add_argument("--apiKey", required=True, type=str,
-                    help="Inform your API Key for the chosen model")
+                    help="Inform your API Key for the chosen model family")
 parser.add_argument("--contextWebPage", required=True, type=str,
                     help="Web page with context content to be used")
 parser.add_argument("--question", required=True, type=str,
                     help="Question about given context")
 args = parser.parse_args()
 
-
 def main():
-
-    #set env vars and create llm as specified
-    if args.model == 'GPT':
+    #set env vars and create llm and embedder as specified
+    if args.modelFamily == 'GPT':
         os.environ['OPENAI_API_KEY'] = args.apiKey
-        llm = OpenAI(temperature=0)
-        embedder = OpenAIEmbeddings()
-    elif args.model == 'Gemini':
+        if not args.LLMModel:
+            model = 'gpt-3.5-turbo'
+        llm = ChatOpenAI(model=model, temperature=0)
+
+        if not args.EmbeddingModel:
+            embeddingModel = 'text-embedding-ada-002'
+        embedder = OpenAIEmbeddings(model=embeddingModel)
+
+    elif args.modelFamily == 'Gemini':
         os.environ['GOOGLE_API_KEY'] = args.apiKey
-        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
-        embedder = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
+        if not args.LLMModel:
+            model = 'gemini-1.5-flash'
+        llm = ChatGoogleGenerativeAI(model=model, temperature=0)
+        if not args.EmbeddingModel:
+            embeddingModel = 'models/text-embedding-004'
+        embedder = GoogleGenerativeAIEmbeddings(model=embeddingModel)
 
     #load texts to be used as a context database
     loader = WebBaseLoader(args.contextWebPage)
